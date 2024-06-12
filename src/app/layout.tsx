109006c4e45a7fceebc96842ac78523e82d9389f
Mateo <teo9515@gmail.com>
2024-06-12 15:12:47 -0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/teologo.svg" />
+        <link rel="icon" href="/teo.svg" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
