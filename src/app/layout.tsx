1e66b2fb4f1d7bc0c8b12c082f87ec89d0cc22ff
Mateo <teo9515@gmail.com>
2024-06-17 17:37:45 -0500

    fix2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/teo.svg" />
+        <meta property="og:title" content="Portfolio Mateo Restrepo" />
+        <meta property="og:description" content="Portfolio Mateo Restrepo" />
+        <meta property="og:url" content="https://rpo95.vercel.app/" />
         <meta
-          property="og:url"
-          content="https://portfolio-rpo95s-projects.vercel.app/"
+          property="og:image"
+          content="https://rpo95.vercel.app/path/to/teo.svg"
         />
-        <meta property="og:image" content="/path/to/teo.svg" />
-        <meta property="og:site_name" content="Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Portfolio Mateo Restrepo" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
